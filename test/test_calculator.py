4f11b3d38a947d3e850ac6639c45453526a93419
vosko <viktoriia.voskoboinyk@hneu.net>
2025-12-10 11:22:37 +0200

    Add CI/CD pipeline

---
+++ test/test_calculator.py
@@ -0,0 +1,16 @@
+import pytest
+from src import calculator
+
+def test_add():
+    assert calculator.add(2, 3) == 5
+
+def test_sub():
+    assert calculator.sub(5, 2) == 3
+
+def test_mul():
+    assert calculator.mul(2, 3) == 6
+
+def test_div():
+    assert calculator.div(6, 3) == 2
+    with pytest.raises(ZeroDivisionError):
+        calculator.div(1, 0)
