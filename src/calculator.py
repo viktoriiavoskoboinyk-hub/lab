87da0b6b072eecfcf19be1d064722a761ae24a6f
vosko <viktoriia.voskoboinyk@hneu.net>
2025-12-10 11:47:35 +0200

    Add CI/CD pipeline

--- src/calculator.py
+++ src/calculator.py
@@ -1,11 +1,14 @@
 def add(a, b):
     return a + b
 
+
 def sub(a, b):
     return a - b
 
+
 def mul(a, b):
     return a * b
+
 
 def div(a, b):
     if b == 0:
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,11 +4,14 @@ from src import calculator
 def test_add():
     assert calculator.add(2, 3) == 5
 
+
 def test_sub():
     assert calculator.sub(5, 2) == 3
 
+
 def test_mul():
     assert calculator.mul(2, 3) == 6
+
 
 def test_div():
     assert calculator.div(6, 3) == 2
